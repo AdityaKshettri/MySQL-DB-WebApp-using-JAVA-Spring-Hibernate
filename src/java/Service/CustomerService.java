7411b66f792b726ec809439d39cbe559713eeb68
AdityaKshettri <adikshettri1623@gmail.com>
2020-03-06 14:17:53 +0530

    first commit

---
+++ src/java/Service/CustomerService.java
@@ -0,0 +1,15 @@
+package Service;
+
+import Entity.Customer;
+import java.util.List;
+
+public interface CustomerService {
+    
+    public List<Customer> getCustomers();
+
+    public void saveCustomer(Customer theCustomer);
+
+    public Customer getCustomer(int theId);
+
+    public void deleteCustomer(int theId);
+}
---
+++ src/java/Service/CustomerServiceImpl.java
@@ -0,0 +1,40 @@
+package Service;
+
+import DAO.CustomerDAO;
+import Entity.Customer;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerDAO customerDAO;
+    
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        return customerDAO.getCustomers();
+    }
+
+    @Override
+    @Transactional
+    public void saveCustomer(Customer theCustomer) {
+        customerDAO.saveCustomer(theCustomer);
+    }
+
+    @Override
+    @Transactional
+    public Customer getCustomer(int theId) {
+        return customerDAO.getCustomer(theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCustomer(int theId) {
+        customerDAO.deleteCustomer(theId);
+    }
+}
